9e7bbc6faa69d8d8f69ab2a5079892b1e1cd8974
Pietro Cinaglia <31215143+pietrocinaglia@users.noreply.github.com>
2023-10-16 14:40:08 +0200

    Example and data, for testing demonstrative only.

--- example/example.py
+++ example/example.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from pymulsim import PyMulSim
+'''
+' Example
+'
+' Note: PyMulSim only reports node similarities greater than 0, other are omitted.
+'''
 
-##
-# Parameters
-##
+from pymulsim import PyMulSim # pip install pymulsim
+
+## Parameters (example)
 #
-workspace = '/Users/imorph/Library/Mobile Documents/com~apple~CloudDocs/Progetti/pyMulSim/'
+workspace = './' # Replace with current folder's path
 #
 source_path = workspace + 'dataset/synt50_1.txt'
 target_path = workspace + 'dataset/synt50_1.txt'
@@ -14,14 +18,17 @@ target_path = workspace + 'dataset/synt50_1.txt'
 layer_ids = [1,2]
 interlayer_ids = [3]
 #
-model_type = 'GIN'
+model_type = 'GNN' # GNN or GIN
 channels = 64
 epochs = 0
-verbose = False
+verbose = True # True: verbose; False: does not show information during computation.
 #
-##
-# Testing
-##
+
+## PyMulSim
 pymulsim = PyMulSim()
+
+## Computing similarities from file (networkx is also supported, see the official repository for help).
 sims = pymulsim.compute_similarities_from_file(source_path, target_path, layer_ids, interlayer_ids, model_type, channels, epochs, verbose)
+
+# Print result
 print(sims)
